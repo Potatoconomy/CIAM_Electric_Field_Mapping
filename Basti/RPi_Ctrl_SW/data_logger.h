d3819da4bb16235bf1c17f46fab14a472b158c43
BastianHarder <93002924+BastianHarder@users.noreply.github.com>
2022-08-24 21:24:30 +0200

    Update data_logger.h
    -adc results are saved in measurements.dat and can be viewed with gnuplot
    -tested version
    -adc results may also be printed in the terminal in one line (always updating) but this has to be uncommented then

--- Basti/RPi_Ctrl_SW/data_logger.h
+++ Basti/RPi_Ctrl_SW/data_logger.h
@@ -21,8 +21,9 @@ class Data_Logger
 public:
     Data_Logger();
 	~Data_Logger();
-	void save_vector_to_file(vector<uint16_t>& u16_vector); 
-	void data_logging(uint32_t spi_clock_speed, double sampleRate);
+	float adc_hex2float(uint16_t adc_result);
+	void save_measurements_to_file(vector<uint16_t>& u16_vector, int timestamp, uint8_t channel); 
+	void data_logging(uint32_t spi_clock_speed, double sampleRate, uint8_t channel);
 
 private:
 
